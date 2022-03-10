364e9503780a328a2b8264caf7e808b8e7123c39
timothyAgevi <timothyagevi@gmail.com>
2022-03-10 11:53:13 +0300

    update mjs

--- index.mjs
+++ index.mjs
@@ -9,11 +9,25 @@ const accBob = await stdlib.newTestAccount(startingBalance);
 const ctcAlice = accAlice.contract(backend);//Alice deploy the application.
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());// Bob attach to it
 
+const HAND = ['Rock', 'Paper', 'Scissors'];//holds aray of hands
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];// holds array of outcomes
+const Player = (Who) => ({// constructor for player implementation
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
 await Promise.all([
   ctcAlice.p.Alice({
     // implement Alice's interact object here
+    ...Player('Alice')
   }),
   ctcBob.p.Bob({
     // implement Bob's interact object here
+    ...Player('Bob')
   }),
 ]);
--- index.rsh
+++ index.rsh
@@ -1,14 +1,21 @@
 'reach 0.1';
-
+//participant interact interface that will be shared between the two players.line3-6
+const Player = {
+    getHand: Fun([], UInt),
+    seeOutcome: Fun([UInt], Null),
+  };
 export const main = Reach.App(() => {
   const Alice = Participant('Alice', {
     // Specify Alice's interact interface here
+    ...Player,
   });
   const Bob   = Participant('Bob', {
    // Specify Bob's interact interface here
+   ...Player,
   });
   init();
   // write your program here
+  Alice.only( ()=>{})
 });
 //building a version of Rock, Paper, Scissors! where two players,
 // Alice and Bob, can wager on the result of the game
