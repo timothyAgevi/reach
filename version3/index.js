02d0e475f3e66d1c0254c22780d301dbe0b5d33e
timothyAgevi <timothyagevi@gmail.com>
2022-03-12 19:09:16 +0300

    set the deadline property based on which connector is being used

--- version3/index.js
+++ version3/index.js
@@ -63,7 +63,7 @@ class Deployer extends Player {
     const ctc = this.props.acc.contract(backend);// call acc.deploy, which triggers a deploy of the contract
     this.setState({view: 'Deploying', ctc});//set the component state to display Deploying display.
     this.wager = reach.parseCurrency(this.state.wager); // UInt provide the wager ,set the wager property
-    this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; // UInt deadline values,
+    this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; // UInt deadline values,set the deadline property based on which connector is being used
     backend.Alice(ctc, this);
     const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
     this.setState({view: 'WaitingForAttacher', ctcInfoStr});
