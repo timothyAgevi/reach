15aa3b250ffbe014de35d9f305bc0cbbed00a5cb
timothyAgevi <timothyagevi@gmail.com>
2022-03-12 19:18:44 +0300

    render the appropriate view from DeployerViews }

--- version3/index.js
+++ version3/index.js
@@ -65,10 +65,10 @@ class Deployer extends Player {
     this.wager = reach.parseCurrency(this.state.wager); // UInt provide the wager ,set the wager property
     this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; // UInt deadline values,set the deadline property based on which connector is being used
     backend.Alice(ctc, this);//start running the Reach program as Alice,
-    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
-    this.setState({view: 'WaitingForAttacher', ctcInfoStr});
+    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);//set the component state to display Waiting for Attacher display
+    this.setState({view: 'WaitingForAttacher', ctcInfoStr});//displays the deployed contract info as JSON
   }
-  render() { return renderView(this, DeployerViews); }
+  render() { return renderView(this, DeployerViews); }//render the appropriate view from DeployerViews
 }
 class Attacher extends Player {
   constructor(props) {
