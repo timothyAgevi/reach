296550778b0a300f503eb97c009bd7573660057e
timothyAgevi <timothyagevi@gmail.com>
2022-03-12 12:44:29 +0300

    update mjs

--- version2/index.mjs
+++ version2/index.mjs
@@ -1,3 +1,21 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
+
+const startingBalance = stdlib.parseCurrency(100);
+const accAlice = await stdlib.newTestAccount(startingBalance);
+const accBob = await stdlib.newTestAccount(startingBalance);
+
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
+
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
 const Player = (Who) => ({
     ...stdlib.hasRandom, // <--- new!,implementation of rabdom that the frontend provides to the backend
     getHand: () => {
